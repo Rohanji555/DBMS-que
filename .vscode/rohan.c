68c5af6175cb9e597e01232262dffdc0317bb8c7
Rohan <rohanshharma5@gmail.com>
2025-07-01 10:57:27 +0530

    Added DBMS answers

---
+++ .vscode/rohan.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int count = 0;  // Counter for even numbers
+    int number = 2; // Starting from the first even number
+
+ printf("hello");
+
+    printf("\n");
+    return 0;
+}
